eb1dc9ef0474a14487d7247c2bbc69ddc35e50b0
angsolovi <sofiaanvi@gmail.com>
2021-10-03 19:56:02 -0500

    lineas de Equipo

--- TorneoFutbolRetoCiclo3.App.Frontend/Startup.cs
+++ TorneoFutbolRetoCiclo3.App.Frontend/Startup.cs
@@ -26,7 +26,7 @@ namespace TorneoFutbolRetoCiclo3.App.Frontend
         {
             services.AddRazorPages();
             //services.AddSingleton<IRepositorioArbitro, RepositorioArbitro>();
-            //services.AddSingleton<IRepositorioEquipo, RepositorioEquipo>();
+            services.AddSingleton<IRepositorioEquipo, RepositorioEquipo>();
             //services.AddSingleton<IRepositorioDesempeno_equipo, RepositorioDesempeno_equipo>();
             //services.AddSingleton<IRepositorioMunicipio, RepositorioMunicipio>();
             //services.AddSingleton<IRepositorioTipo_novedad, RepositorioTipo_novedad>();
