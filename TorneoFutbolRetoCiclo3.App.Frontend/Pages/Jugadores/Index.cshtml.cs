9addffc13e82a9f15f33e6fce73b5997a3a541a2
JhonMontoya <jonsujohannes@gmail.com>
2021-10-09 14:52:26 -0500

    Correccion

--- TorneoFutbolRetoCiclo3.App.Frontend/Pages/Jugadores/Index.cshtml.cs
+++ TorneoFutbolRetoCiclo3.App.Frontend/Pages/Jugadores/Index.cshtml.cs
@@ -16,8 +16,8 @@ namespace TorneoFutbolRetoCiclo3.App.Frontend.Pages.Jugadores
         private readonly IRepositorioPosicion_jugador _repoPosJugador;
 
         public IEnumerable<Jugador> jugadores {get; set;}
-        public Equipo equipo {get;set;}
-        public Posicion_jugador PosJugador {get; set;}
+        public IEnumerable <Equipo> equipos {get;set;}
+        public IEnumerable <Posicion_jugador> PosJugadores {get; set;}
 
         public IndexModel(IRepositorioJugador repoJugador, IRepositorioEquipo repoEquipo, IRepositorioPosicion_jugador repoPosicion_jugador)
         {
@@ -29,11 +29,8 @@ namespace TorneoFutbolRetoCiclo3.App.Frontend.Pages.Jugadores
         public void OnGet()
         {
             jugadores = _repoJugador.GetAllJugadores();
-            foreach (var player in jugadores)
-            {
-                equipo = _repoEquipo.GetEquipo(player.EquipoId);
-                PosJugador = _repoPosJugador.GetPosicion_jugador(player.Posicion_jugadorId);
-            }
+            equipos = _repoEquipo.GetAllEquipos();
+            PosJugadores = _repoPosJugador.GetAllPosicion_jugadores();
         }
     }
 }
