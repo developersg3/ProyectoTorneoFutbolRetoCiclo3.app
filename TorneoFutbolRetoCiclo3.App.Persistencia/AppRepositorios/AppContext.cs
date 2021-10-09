8eac1a65e61e4b8e49bc85acec9bae3bf3b0f75d
JhonMontoya <jonsujohannes@gmail.com>
2021-10-09 12:01:12 -0500

    Modificaciones

--- TorneoFutbolRetoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
+++ TorneoFutbolRetoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
@@ -20,7 +20,7 @@ namespace TorneoFutbolRetoCiclo3.App.Persistencia
     {
       if (!optionsBuilder.IsConfigured)
       {
-        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TorneoFutbolPrueba");        
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TorneoFutbolPrueba");
       }
     }
   }
