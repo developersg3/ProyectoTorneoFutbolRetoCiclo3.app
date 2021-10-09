9fb11dccde8138101f7fe0fc7a1b5834370cd45b
JhonMontoya <jonsujohannes@gmail.com>
2021-10-08 22:55:38 -0500

    Listo FK

--- TorneoFutbolRetoCiclo3.App.Frontend/Pages/Jugadores/Index.cshtml.cs
+++ TorneoFutbolRetoCiclo3.App.Frontend/Pages/Jugadores/Index.cshtml.cs
@@ -13,13 +13,17 @@ namespace TorneoFutbolRetoCiclo3.App.Frontend.Pages.Jugadores
     {
         private readonly IRepositorioJugador _repoJugador;
         private readonly IRepositorioEquipo _repoEquipo;
+        private readonly IRepositorioPosicion_jugador _repoPosJugador;
+
         public IEnumerable<Jugador> jugadores {get; set;}
         public Equipo equipo {get;set;}
+        public Posicion_jugador PosJugador {get; set;}
 
-        public IndexModel(IRepositorioJugador repoJugador, IRepositorioEquipo repoEquipo)
+        public IndexModel(IRepositorioJugador repoJugador, IRepositorioEquipo repoEquipo, IRepositorioPosicion_jugador repoPosicion_jugador)
         {
             _repoJugador = repoJugador;
             _repoEquipo = repoEquipo;
+            _repoPosJugador = repoPosicion_jugador;
         }
 
         public void OnGet()
@@ -27,7 +31,8 @@ namespace TorneoFutbolRetoCiclo3.App.Frontend.Pages.Jugadores
             jugadores = _repoJugador.GetAllJugadores();
             foreach (var player in jugadores)
             {
-             equipo = _repoEquipo.GetEquipo(player.EquipoId);   
+                equipo = _repoEquipo.GetEquipo(player.EquipoId);
+                PosJugador = _repoPosJugador.GetPosicion_jugador(player.Posicion_jugadorId);
             }
         }
     }
